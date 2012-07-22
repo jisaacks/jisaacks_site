d48f08df70ae7232d197bcd4f6154f599f137875
John Isaacks <john.isaacks@programming-perils.com>
2012-07-22 11:29:13 -0400

    Add default scope for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ActiveRecord::Base
   attr_accessible :content, :title
   validates :title, :presence => true
   validates :content, :presence => true
+  default_scope :order => 'created_at DESC'
 end
