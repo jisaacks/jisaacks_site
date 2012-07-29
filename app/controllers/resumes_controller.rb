77dc73a8751708aa004d53ed37988c9ed4f76b87
John Isaacks <john.isaacks@programming-perils.com>
2012-07-29 15:22:41 -0400

    clean up pdf options

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -3,7 +3,18 @@ class ResumesController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.pdf { render(:pdf => "resume", :layout => "pdf.html.haml", :template => "resumes/index.pdf.haml", :show_as_html => params[:debug].present?, :zoom => 0.75, :encoding => 'utf-8', :page_size => 'letter') }
+      format.pdf do
+        pdf_options = {
+          pdf:          "resume",
+          layout:       "pdf.html.haml",
+          template:     "resumes/index.pdf.haml",
+          show_as_html: params[:debug].present?,
+          zoom:         0.75, 
+          encoding:     'utf-8',
+          page_size:    'letter'
+        }
+        render(pdf_options)
+      end
     end
   end
 
