c741d8c5f3a85a4816eb5b30092af87c65b76718
John Isaacks <john.isaacks@programming-perils.com>
2012-07-29 15:19:30 -0400

    Make resume downloadable as PDF

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'friendly_id', '~> 4.0.1'
 
 gem 'thin'
 
+gem 'wicked_pdf'
+
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,6 +1,10 @@
 class ResumesController < ApplicationController
 
   def index
+    respond_to do |format|
+      format.html
+      format.pdf { render(:pdf => "resume", :layout => "pdf.html.haml", :template => "resumes/index.pdf.haml", :show_as_html => params[:debug].present?, :zoom => 0.75, :encoding => 'utf-8', :page_size => 'letter') }
+    end
   end
 
 end
