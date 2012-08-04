d9dd10494cb49ef68146c86bf8eb09924ccf5a1a
John Isaacks <john.isaacks@programming-perils.com>
2012-08-03 23:44:29 -0400

    Make test env require a password fixes #9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   check_authorization :if => :admin_namespace?
+  if ENV['RACK_ENV'] == 'test'
+    before_filter :authenticate_user!
+  end
 
   def admin_namespace?
     params[:controller].split("/").first == "admin"
