e47f362214748646690c099ae2ebb5bd7f46dd66
JD Isaacks <jd@jisaacks.com>
2013-03-12 21:47:47 -0400

    Add spam math question to help revent spam comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,10 +4,12 @@ class Comment < ActiveRecord::Base
   rakismet_attrs :author => :name, :author_email => :email, :author_url => :website
   belongs_to :commentable, :polymorphic => true
   has_many :comments, :as => :commentable
-  attr_accessible :comment, :email, :name, :website
+  attr_accessible :comment, :email, :name, :website, :spam_math
+  attr_accessor :spam_math
   validates :name,    :presence => true
   validates :email,   :presence => true
   validates :comment, :presence => true
+  validate :spam_math_is_correct
   after_create :notify
 
   def article
@@ -20,5 +22,10 @@ class Comment < ActiveRecord::Base
 
   def notify
     Mailer.new_comment(self).deliver
+  end
+
+  private
+  def spam_math_is_correct
+     errors.add(:spam_math, 'needs a correct answer') unless self.spam_math.to_i == 7
   end
 end
